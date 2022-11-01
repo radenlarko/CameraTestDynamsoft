9157b22c35a74d36e8a6a6e16a48e8be0a0d6392
radenlarko <radenlarko@gmail.com>
2022-11-01 09:12:32 +0700

    Show QR Code overlays Install RN svg

--- src/screens/CameraTest/CameraQR.tsx
+++ src/screens/CameraTest/CameraQR.tsx
@@ -1,7 +1,7 @@
 import {useIsFocused} from '@react-navigation/native';
 import * as React from 'react';
 
-import {StyleSheet, Text, View} from 'react-native';
+import {Dimensions, Platform, StyleSheet, Text, View} from 'react-native';
 import {
   Camera,
   useCameraDevices,
@@ -13,9 +13,12 @@ import {
   TextResult,
 } from 'vision-camera-dynamsoft-barcode-reader';
 import * as REA from 'react-native-reanimated';
+import { Polygon, Text as SVGText, Svg, Rect } from 'react-native-svg';
 
 const CameraQR = (): JSX.Element => {
   const [hasPermission, setHasPermission] = React.useState(false);
+  const [frameWidth, setFrameWidth] = React.useState(1280);
+  const [frameHeight, setFrameHeight] = React.useState(720);
   const [barcodeResults, setBarcodeResults] = React.useState(
     [] as TextResult[],
   );
@@ -40,6 +43,41 @@ const CameraQR = (): JSX.Element => {
     })();
   }, []);
 
+  const getPointsData = (tr: TextResult) => {
+    var pointsData = tr.x1 + ',' + tr.y1 + ' ';
+    pointsData = pointsData + tr.x2 + ',' + tr.y2 + ' ';
+    pointsData = pointsData + tr.x3 + ',' + tr.y3 + ' ';
+    pointsData = pointsData + tr.x4 + ',' + tr.y4;
+    return pointsData;
+  };
+
+  const getViewBox = () => {
+    const frameSize = getFrameSize();
+    const viewBox = '0 0 ' + frameSize[0] + ' ' + frameSize[1];
+    return viewBox;
+  };
+
+  const getFrameSize = (): number[] => {
+    let width: number, height: number;
+    if (Platform.OS === 'android') {
+      if (
+        frameWidth > frameHeight &&
+        Dimensions.get('window').width > Dimensions.get('window').height
+      ) {
+        width = frameWidth;
+        height = frameHeight;
+      } else {
+        console.log('Has rotation');
+        width = frameHeight;
+        height = frameWidth;
+      }
+    } else {
+      width = frameWidth;
+      height = frameHeight;
+    }
+    return [width, height];
+  };
+
   return (
     <View style={{flex: 1}}>
       {device != null && hasPermission && (
@@ -58,6 +96,30 @@ const CameraQR = (): JSX.Element => {
           ))}
         </>
       )}
+      <Svg style={StyleSheet.absoluteFill} viewBox={getViewBox()}>
+        {barcodeResults.map((barcode, idx) => (
+          <Polygon
+            key={'poly-' + idx}
+            points={getPointsData(barcode)}
+            fill="lime"
+            stroke="green"
+            opacity="0.5"
+            strokeWidth="1"
+          />
+        ))}
+        {barcodeResults.map((barcode, idx) => (
+          <SVGText
+            key={'text-' + idx}
+            fill="white"
+            stroke="purple"
+            fontSize={(getFrameSize()[0] / 400) * 20}
+            fontWeight="bold"
+            x={barcode.x1}
+            y={barcode.y1}>
+            {barcode.barcodeText}
+          </SVGText>
+        ))}
+      </Svg>
     </View>
   );
 };
